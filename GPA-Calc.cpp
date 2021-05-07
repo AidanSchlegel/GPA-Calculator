e89d2666b5a3c4c29f74504e6f120ac252c1bf23
AidanSchlegel <80399774+AidanSchlegel@users.noreply.github.com>
2021-05-07 13:49:22 -0400

    Update GPA-Calc.cpp

--- GPA-Calc.cpp
+++ GPA-Calc.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 
 int namespace std;
-int main(){
+int main()
+{
+        char q;
+        float grades, gpa;
+        cout << "enter the total grades:";
+        cin >> grades;
+
+        if (grades > 80)
+                gpa = 4.0;
+        else if (grades < 50)
+                gpa = 0.0;
+        else
+                gpa = 2*((grades-50)/1.5)*0.1;
+                cout << "gpa=" << gpa;
+
+
+        cin >> q;
+        return 0;
+}
